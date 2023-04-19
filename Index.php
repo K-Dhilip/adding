d4ddbdc95f2490e3cc2121d9593574f047f12a3b
unknown <dhilipweb@gmail.com>
2023-04-19 14:20:53 +0200

    adadadddd

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <form action="model.php" method ="post">
+        <input type="number" name="one" id="">
+        <input type="number" name="two" id="">
+        <button type="submit">Envoyer</button>
+    </form>
+
+
+    <!-- <?php
+    include('./model.php');
+    echo $total;
+    ?> -->
+</body>
+</html>
---
+++ model.php
@@ -0,0 +1,31 @@
+<?php 
+
+ echo "Bonjour ".$_GET["one"] . $_GET["two"];
+
+// echo $a = $_GET["value1"], $b = $_GET["value2"]; 
+// echo $_GET["value1"],  $_GET["value2"]; 
+
+
+
+// class calcul {
+
+// public $a = $_GET["value1"], $b = $_GET["value2"]; 
+
+
+// public function Sum($a,$b) {
+//     return $this->a + $this->b;
+// }
+
+// }
+// $calculation = new Calcul();
+// $total = $calculation->Sum();
+
+
+// ?>
+
+
+
+
+
+
+
